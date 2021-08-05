5b739686478b4a45af1329405481166379b63121
Yris Siqueira <siqueirayris@hotmail.com>
2021-08-05 12:57:30 -0300

    Put decorator

---
+++ NetworkKit/Sources/NetworkKit/RequestConfigHeadersDecorator.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public final class RequestConfigHeadersDecorator {
+    private var instance: RequestConfig
+//    private let userManager: UserManagerProtocol
+
+    // MARK: - Initialization
+
+    public init(_ instance: RequestConfig/*, userManager: UserManagerProtocol = UserManager()*/) {
+        self.instance = instance
+//        self.userManager = userManager
+    }
+}
+
+extension RequestConfigHeadersDecorator: NetworkRouteProtocol {
+    public var config: RequestConfigProtocol {
+//        if let jwt = userManager.retrieveJWT() {
+            instance.headers["header"] = "some-header" /// you cand add jwt for example
+//        }
+
+        return instance
+    }
+}
--- NetworkSample/NetworkSample/Service/SomeServiceRoute.swift
+++ NetworkSample/NetworkSample/Service/SomeServiceRoute.swift
@@ -11,9 +11,11 @@ enum SomeServiceRoute: NetworkRouteProtocol {
     }
 
     private func configureRequest() -> RequestConfigProtocol {
-        return RequestConfig(host: "aquele-mock.herokuapp.com",
-                             path: "/some-data",
-                             method: .get,
-                             debugMode: true) /// The default is false, but when enabled shows the curl text in console
+        let config = RequestConfig(host: "aquele-mock.herokuapp.com",
+                                   path: "/some-data",
+                                   method: .get,
+                                   debugMode: true) /// The default is false, but when enabled shows the curl text in console
+
+        return RequestConfigHeadersDecorator(config).config
     }
 }
